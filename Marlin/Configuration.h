e2bc7d01d7c50b5fecc32f979514172ba7a5e83d
padic-dev <39010826+padic-dev@users.noreply.github.com>
2021-04-17 01:00:01 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -507,13 +507,13 @@
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify between 1 and HOTENDS values per array.
     // If fewer than EXTRUDER values are provided, the last element will be repeated.
-    #define DEFAULT_Kp_LIST {  22.20,  22.20 }
-    #define DEFAULT_Ki_LIST {   1.08,   1.08 }
-    #define DEFAULT_Kd_LIST { 114.00, 114.00 }
+    #define DEFAULT_Kp_LIST {  27.39,  27.39 }
+    #define DEFAULT_Ki_LIST {   2.23,   2.23 }
+    #define DEFAULT_Kd_LIST {  83.97,  83.97 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp 27.39
+    #define DEFAULT_Ki  2.23
+    #define DEFAULT_Kd 83.97
   #endif
 #endif // PIDTEMP
 
@@ -550,11 +550,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 149.21
+  #define DEFAULT_bedKi  29.09
+  #define DEFAULT_bedKd 510.30
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
